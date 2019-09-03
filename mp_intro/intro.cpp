7db9b81c8a7f36999c4e29b6964fec1428e1cf5f
Joseph Lee <tlee95@illinois.edu>
2019-09-03 15:19:32 -0500

    changes to intro mp

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -6,6 +6,16 @@
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+  cs225::PNG png, result;
+  png.readFromFile(inputFile);
+  int centerY = (int) png.height/2;
+  for(int x = 0; x <= png.width; x++){
+    for(int y = 0; y <= png.height; y++) {
+      cs225::HSLAPixel & pixel =  result.getPixel(x,y);
+      pixel._copy(png.getPixel(x,height-y));
+    }
+  }
+  png.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
